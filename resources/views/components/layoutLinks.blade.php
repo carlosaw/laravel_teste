a7e5080b2a3cb784ee37bf7151badc005c6955a8
carlosaw <carlosfreevg@gmail.com>
2023-04-18 20:58:16 -0400

    Serviços

--- resources/views/components/layoutLinks.blade.php
+++ resources/views/components/layoutLinks.blade.php
@@ -1,7 +1,7 @@
 @if (request()->is('vehicles/new') || request()->is('vehicles/edit'))
 <div class="btn-group">
-  <x-btnselect btnHref='/vehicles' btnText='Veículos'></x-btnselect>
   <x-btn btnHref='/clients' btnText='Clientes'></x-btn>
+  <x-btnselect btnHref='/vehicles' btnText='Veículos'></x-btnselect>
   <x-btn btnHref='/addresses' btnText='Endereços'></x-btn>
   <x-btn btnHref='/products' btnText='Produtos'></x-btn>
   <x-btn btnHref='/orders' btnText='Serviços'></x-btn>
@@ -13,8 +13,8 @@
 
 @elseif (request()->is('clients/new') || request()->is('clients/edit'))
 <div class="btn-group">
-  <x-btn btnHref='/vehicles' btnText='Veiculos'></x-btn>
   <x-btnselect btnHref='/clients' btnText='Clientes'></x-btnselect>
+  <x-btn btnHref='/vehicles' btnText='Veiculos'></x-btn> 
   <x-btn btnHref='/addresses' btnText='Endereços'></x-btn>
   <x-btn btnHref='/products' btnText='Produtos'></x-btn>
   <x-btn btnHref='/orders' btnText='Serviços'></x-btn>
@@ -26,8 +26,8 @@
 
 @elseif (request()->is('addresses/new') || request()->is('addresses/edit'))
 <div class="btn-group">
-  <x-btn btnHref='/vehicles' btnText='Veiculos'></x-btn>
   <x-btn btnHref='/clients' btnText='Clientes'></x-btn>
+  <x-btn btnHref='/vehicles' btnText='Veiculos'></x-btn>
   <x-btnselect btnHref='/addresses' btnText='Endereços'></x-btnselect>
   <x-btn btnHref='/products' btnText='Produtos'></x-btn>
   <x-btn btnHref='/orders' btnText='Serviços'></x-btn>
@@ -39,8 +39,8 @@
 
 @elseif (request()->is('products/new') || request()->is('products/edit'))
 <div class="btn-group">
-  <x-btn btnHref='/vehicles' btnText='Veiculos'></x-btn>
   <x-btn btnHref='/clients' btnText='Clientes'></x-btn>
+  <x-btn btnHref='/vehicles' btnText='Veiculos'></x-btn>
   <x-btn btnHref='/addresses' btnText='Endereços'></x-btn>
   <x-btnselect btnHref='/products' btnText='Produtos'></x-btnselect>
   <x-btn btnHref='/orders' btnText='Serviços'></x-btn>
@@ -51,9 +51,9 @@
 </div>
 
 @elseif (request()->is('orders/new'))
-<div class="btn-group">
-  <x-btn btnHref='/vehicles' btnText='Veiculos'></x-btn>
+<div class="btn-group">  
   <x-btn btnHref='/clients' btnText='Clientes'></x-btn>
+  <x-btn btnHref='/vehicles' btnText='Veiculos'></x-btn>
   <x-btn btnHref='/addresses' btnText='Endereços'></x-btn>
   <x-btn btnHref='/products' btnText='Produtos'></x-btn>
   <x-btnselect btnHref='/orders' btnText='Serviços'></x-btnselect>
@@ -64,9 +64,9 @@
 </div>
 
 @elseif (request()->is('mechanics/new') || request()->is('mechanics/edit'))
-<div class="btn-group">
-  <x-btn btnHref='/vehicles' btnText='Veiculos'></x-btn>
+<div class="btn-group">  
   <x-btn btnHref='/clients' btnText='Clientes'></x-btn>
+  <x-btn btnHref='/vehicles' btnText='Veiculos'></x-btn>
   <x-btn btnHref='/addresses' btnText='Endereços'></x-btn>
   <x-btn btnHref='/products' btnText='Produtos'></x-btn>
   <x-btn btnHref='/orders' btnText='Serviços'></x-btn>
@@ -74,5 +74,5 @@
   <div class="user-logout">
     <a href="{{route('logout')}}">Sair</a>  
   </div> 
-<div class="btn-group">         
+</div>         
 @endif      
